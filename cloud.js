dd22c655d86f89963a6edc63ff22400cd7f3e095
Oscar Lundberg <info@oscarlundberg.com>
2021-09-12 13:16:46 +0200

    created basic backend

--- cloud.js
+++ cloud.js
@@ -1,3 +1,14 @@
 handlers.list_lobby_events = function (args, context) {
 
+    var lobby_events = server.GetTitleData([args.Lobby]) || [];
+
+    return { events: lobby_events };
+};
+
+handlers.send_lobby_event = function (args, context) {
+
+    var lobby_events = server.GetTitleData([args.Lobby]) || [];
+    lobby_events.push(args.event);
+    server.SetTitleData(args.Lobby, lobby_events);
+    return { events: lobby_events };
 }
