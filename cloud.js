3aafa3b99a89c9ef84d40dd25e015e455f0b0c91
Oscar Lundberg <info@oscarlundberg.com>
2021-09-12 19:02:32 +0200

    cleanup backend

--- cloud.js
+++ cloud.js
@@ -1,31 +1,40 @@
+const read = function (key) {
+    return JSON.parse(server.GetTitleData()[key] || "[]");
+}
+
+const write = function (key, val) {
+    let request = { key, value: JSON.stringify(val) }
+    server.SetTitleData(request);
+}
+
+
 handlers.list_lobby_events = function (args, context) {
 
-    var lobby_events = JSON.parse(server.GetTitleData([args.Lobby]) || "[]");
+    var lobby_events = read(args.Lobby);
     return { events: lobby_events };
 };
 
 handlers.send_lobby_event = function (args, context) {
 
-    var lobby_events = JSON.parse(server.GetTitleData()[args.Lobby] || "[]");
+    var lobby_events = read(args.Lobby);
     lobby_events.push(args.Payload);
-    server.SetTitleData(args.Lobby, JSON.stringify(lobby_events));
+    write(args.Lobby, lobby_events);
     return { events: lobby_events };
 }
 
 handlers.create_lobby = function (args, context) {
-    let lobbies = JSON.parse(server.GetTitleData([])["lobbies"] || "[]");
-    console.log(lobbies);
+    let lobbies = read("lobbies");
     lobbies.push(args.Payload);
-    server.SetTitleData("lobbies", JSON.stringify(lobbies));
+    write(lobbies);
     return { success: true };
 }
 
 handlers.get_lobbies = function (args, context) {
-    return { lobbies: JSON.parse(server.GetTitleData(["lobbies"])) };
+    return { lobbies: read("lobbies") };
 }
 
 handlers.join_lobby = function (args, context) {
-    let lobbies = JSON.parse(server.GetTitleData(["lobbies"]) || "[]");
+    let lobbies = read("lobbies");
     for (let i = 0; i < lobbies.length; i++) {
         if (lobbies[i].id === args.Lobby) {
             var lobbyData = lobbies[i];
