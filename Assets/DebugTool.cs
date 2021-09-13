9980a69bcad36e9aa667d8d0e86a946ad00f45fc
Oscar Lundberg <info@oscarlundberg.com>
2021-09-13 18:43:31 +0200

    added debug tools

---
+++ Assets/DebugTool.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DebugTool : MonoBehaviour
+{
+    public HostMethods hm;
+    public void SendMessage(InputField inp)
+    {
+        hm.SendMessage(inp.text);
+    }
+}
